40510e9f53a566e056473f172e7d098b69a716c1
Paul Onyekwelu <silanka@CEO-MacBook-Pro.local>
2020-09-27 18:47:06 +0100

    implementation of filesystem storage of note files

--- app.mjs
+++ app.mjs
@@ -10,11 +10,13 @@ import rfs from 'rotating-file-stream';
 import { __dirname } from "./approotdir.mjs";
 import { normalizePort, onError, onListening, handle404, basicErrorHandler } from "./appsupport.mjs";
 import { InMemoryNotesStore } from "./models/notes-memory.mjs";
+import { FsNotesStore } from "./models/notes-fm.mjs";
 import { router as indexRouter } from "./routes/index.mjs";
 import { router as notesRouter } from './routes/notes.mjs';
 
 const app = express();
-export const NotesStore = new InMemoryNotesStore();
+// export const NotesStore = new InMemoryNotesStore();
+export const NotesStore = new FsNotesStore();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
